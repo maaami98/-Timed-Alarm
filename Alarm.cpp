9298013a63092f56e30afa6fd5e23e0b97bae37c
maaami98 <35247648+maaami98@users.noreply.github.com>
2018-01-09 06:45:07 +0300

    Add files via upload

---
+++ Alarm.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+#include <ctime>
+
+#include <windows.h>
+using namespace std;
+int main() {
+    std::clock_t start;
+    int sure;
+
+     //süreye þu anki zamaný atadý
+
+    setlocale(LC_ALL,"Turkish");
+	cout<<"Alarým Süresini Giriniz\n";
+	cout<<"Dakika:";
+	int dk;
+	cin>>dk;
+	cout<<"Saniye:";
+	int sn;
+	cin>>sn;
+	sn=sn+(dk*60);
+	start = std::clock();
+	while(1){
+    sure = ( std::clock() - start ) /  CLOCKS_PER_SEC;
+    system("cls");
+    cout<<"Alarm Kuruldu.\n";
+    switch(sure%4)
+    {
+    	case 0:{cout<<"-";
+			break;
+		}
+		case 1:{cout<<"\\";
+			break;
+		}
+		case 2:{cout<<"|";
+			break;
+		}
+		case 3:{cout<<"/";
+			break;
+		}
+	}
+	printf("  %d:%d",(sn-sure)/60,(sn-sure)%60);
+	if(sure==sn)break;
+	Sleep(1000);
+	}
+	PlaySound("alarm.wav",NULL, SND_FILENAME|SND_LOOP);
+  
+}
